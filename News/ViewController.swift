d5ac240cd7f1e72460ce25859c01e5ecf57968b0
Maksim Ozhgibecov <93679282+ozhgiMak159@users.noreply.github.com>
2022-10-28 15:59:25 +0500

    Initial Commit

---
+++ News/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  News
+//
+//  Created by Maksim  on 28.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
